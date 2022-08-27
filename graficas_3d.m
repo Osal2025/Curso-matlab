8871cce4ed201d89342acf1d2d00b160d5b7d44d
Osal2025 <111815408+Osal2025@users.noreply.github.com>
2022-08-27 09:07:42 -0500

    Add files via upload

---
+++ graficas_3d.m
@@ -0,0 +1,27 @@
+clc
+clear all
+close all
+
+% [x,y] = meshgrid(-2:0.1:2, -2:0.1:2);
+
+% z = sqrt(16 - x.^2 - y.^2); %Funcion z(x,y)
+
+% figure(1)
+
+% surf(x,y,z, 'FaceAlpha',0.5, 'EdgeColor', 'none')
+% 
+% hold on
+
+% zz = x + y;
+
+% surf(x,y,zz, 'FaceAlpha',0.5, 'EdgeColor', 'none')
+
+% hold off
+
+[x,y] = meshgrid(-5:0.5:5, -5:0.5:5);
+
+z = sin(x+y); %Funcion z(x,y)
+
+figure(1)
+
+surf(x,y,z)
